86e2127c08477aa669040cfc680978638388c6d3
praneeth <you@example.com>
2023-01-11 10:43:48 +0530

    youtube API

--- yoga-admin/src/main/java/com/traditional/yoga/YogaApplication.java
+++ yoga-admin/src/main/java/com/traditional/yoga/YogaApplication.java
@@ -2,6 +2,8 @@ package com.traditional.yoga;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class YogaApplication {
@@ -10,4 +12,12 @@ public class YogaApplication {
 		SpringApplication.run(YogaApplication.class, args);
 	}
 
+	@Bean
+	public RestTemplate getTemplate() {
+		
+		return new RestTemplate();
+		
+	}
+	 
 }
+
---
+++ yoga-admin/src/main/java/com/traditional/yoga/controller/YoutubeController.java
@@ -0,0 +1,65 @@
+package com.traditional.yoga.controller;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@CrossOrigin("*")
+@RestController
+@RequestMapping(value = "/video", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+public class YoutubeController {
+	 @Autowired
+	 private RestTemplate restTemplate;
+	    @GetMapping("/youtube-data-api")
+	
+	    public Object youTube(@RequestParam String key,@RequestParam String part,@RequestParam String q,
+	            @RequestParam String channelId,@RequestParam String type,@RequestParam String maxResults)throws Exception
+	    {
+	        if (q == null || q.trim().isEmpty()) {
+	            throw new IllegalArgumentException("'q' parameter must be provided");
+	        }
+	        if (channelId == null || channelId.trim().isEmpty()) {
+	            throw new IllegalArgumentException("'channelId' parameter must be provided");
+	        }
+	        String url="https://www.googleapis.com/youtube/v3/search?key={key}&part={part}&q={q}&channelId={channelId}&type={type}&maxResults={maxResults}";
+	        Map<String,String>params=new HashMap<>();
+	        params.put("key", key);
+	        params.put("part", part);
+	        params.put("q", q);
+	        params.put("channelId", channelId);
+	        params.put("type", type);
+	        params.put("maxResults", maxResults);
+	        return restTemplate.getForObject(url, Object.class, params);
+	    }
+
+	    @GetMapping("/youtube-live-stream-api")
+	    public Object liveStream(@RequestParam String key,@RequestParam String part,@RequestParam String q,
+	            @RequestParam String channelId)throws Exception
+	    {
+	        if (q == null || q.trim().isEmpty()) {
+	            throw new IllegalArgumentException("'q' parameter must be provided");
+	        }
+	        if (channelId == null || channelId.trim().isEmpty()) {
+	            throw new IllegalArgumentException("'channelId' parameter must be provided");
+	        }
+	        String url="https://www.googleapis.com/youtube/v3/liveBroadcasts?key={key}&part={part}&q={q}&channelId={channelId}";
+	        Map<String,String>params=new HashMap<>();
+	        params.put("key", key);
+	        params.put("part", part);
+	        params.put("q", q);
+	        params.put("channelId", channelId);
+	        return restTemplate.getForObject(url, Object.class, params);
+	    }
+	    
+	    
+	    
+	    
+}
