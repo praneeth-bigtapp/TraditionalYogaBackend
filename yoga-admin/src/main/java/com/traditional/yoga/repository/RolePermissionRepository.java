c17e1d5c23da0b4c1fa15b21de0880b6afc01c66
Sumukesh <amanchasumukesh08@gmail.com>
2023-01-11 15:45:31 +0530

    Role permission

--- yoga-admin/src/main/java/com/traditional/yoga/repository/RolePermissionRepository.java
+++ yoga-admin/src/main/java/com/traditional/yoga/repository/RolePermissionRepository.java
@@ -2,6 +2,8 @@ package com.traditional.yoga.repository;
 
 import java.util.List;
 
+import javax.transaction.Transactional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -11,7 +13,7 @@ import org.springframework.stereotype.Repository;
 import com.traditional.yoga.model.RolePermissionModel;
 
 @Repository
-public interface RolePermissionRepository extends JpaRepository<RolePermissionModel, Integer> {
+public interface RolePermissionRepository extends JpaRepository<RolePermissionModel, Long> {
 
 	@Query(value = "SELECT * FROM `role_permissions` WHERE `role_Id`= :roleId AND `permission_id`<>6", nativeQuery = true)
 	List<RolePermissionModel> getPermissionByroleId(@Param("roleId") int roleId);
@@ -22,8 +24,14 @@ public interface RolePermissionRepository extends JpaRepository<RolePermissionMo
 	@Query(value = "SELECT COUNT(*) FROM `role_permissions` WHERE `role_Id`= :roleId AND `sub_module_id` = :subModuleId AND permission_id<>6", nativeQuery = true)
 	int findCountOfSubModulesPermissions(@Param("roleId") int roleId, @Param("subModuleId") int subModuleId);
 	
+//	@Transactional
+//	@Modifying
+//	@Query("DELETE FROM role_permissions WHERE role_id = :roleId")
+//	void deleteByRoleId(@Param("roleId") int roleId);
+	
 	@Modifying
-	@Query("DELETE FROM `role_permissions` WHERE `role_id` = :roleId")
-	void deleteByRoleId(@Param("roleId") int roleId);
+	@Transactional
+	@Query(value = "DELETE FROM role_permissions WHERE role_id = ?1 ", nativeQuery = true)
+	void deleteByRoleId(int roleId);
 
 }
